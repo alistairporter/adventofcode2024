1400c7604bb3fc576d6dd0e4f8b113caf228c067
Alistair Porter <alistair_porter@outlook.com>
2024-12-01 17:48:25 +0000

    part2 done!

---
+++ day01/part2/day01.rs
@@ -0,0 +1,46 @@
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+    path::Path,
+};
+
+fn lines_from_file(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
+    BufReader::new(File::open(filename)?).lines().collect()
+}
+
+fn main() {
+    let lines = lines_from_file("./input.txt").expect("Could not load lines");
+    let mut list1 = vec![];
+    let mut list2 = vec![];
+    let mut similarity = 0;
+    
+    for line in lines {        
+        // println!("{:?}", line);
+        let splitline: Vec<&str> = line.split("   ").collect();
+        let var0: i32 = splitline[0].parse().expect("Failed to parse string to integer");
+        let var1: i32 = splitline[1].parse().expect("Failed to parse string to integer");
+        list1.push(var0);
+        list2.push(var1);
+    }
+    
+    list1.sort();
+    list2.sort();
+//    println!("list1 contents:");
+//    for item in list1 {
+//        println!("{:?}", item);
+//    }
+//    println!("list2 contents:");
+//    for item in list2 {
+//        println!("{:?}", item);
+//    }
+//    for (pos, e) in (&list1).iter().enumerate() {
+//        println!("Element at position {}: {:?}", pos, e);
+//        let diff = list2[pos].abs_diff(*e);
+//        total_diff = total_diff + diff;
+//    }
+    for item in list1 {
+        let list_occ_count = list2.iter().filter(|&n| *n == item).count() as i32;
+        similarity = similarity + (item * list_occ_count)
+    }
+    println!("{}",similarity);
+}
